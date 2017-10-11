548132a4a7ae35accb65b2cc513f8d454cf842a0
Roland Schäfer <roland.schaefer@fu-berlin.de>
2017-10-11 21:22:16 +0200

    Loading/blacklisting implemented.

--- Data/R/load.R
+++ Data/R/load.R
@@ -1,85 +1,121 @@
-e1s.nole <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+.txt'))
-e1s.e <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+e.txt'))
-e1s.en <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+en.txt'))
-e1s.er <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+er.txt'))
-e1s.n <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+n.txt'))
-e1s.s <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+s.txt'))
-e1s.Ue <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+Ue.txt'))
-e1s.Uer <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+Uer.txt'))
-# e1s.Un <- read.csv2(paste0(data.path, 'erstglieder/erstglieder+Un.txt'))  # No matches.
 
-e1hapax.nole <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+.csv'),
-                          sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1hapax.e <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+e.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1hapax.en <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+en.csv'),
-                        sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1hapax.er <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+er.csv'),
-                        sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1hapax.n <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+n.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1hapax.s <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+s.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1hapax.Ue <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+Ue.csv'),
-                        sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1hapax.Uer <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+Uer.csv'),
-                         sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-# e1hapax.Un <- read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+Un.csv'),
-#                         sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))  # No matches.
+nouns <- list(
+  no  = read.csv2(paste0(data.path, 'erstglieder/erstglieder+.txt'),
+                  header = FALSE, col.names = c("N1")),
+  e   = read.csv2(paste0(data.path, 'erstglieder/erstglieder+e.txt'),
+                  header = FALSE, col.names = c("N1")),
+  en  = read.csv2(paste0(data.path, 'erstglieder/erstglieder+en.txt'),
+                  header = FALSE, col.names = c("N1")),
+  er  = read.csv2(paste0(data.path, 'erstglieder/erstglieder+er.txt'),
+                  header = FALSE, col.names = c("N1")),
+  n   = read.csv2(paste0(data.path, 'erstglieder/erstglieder+n.txt'),
+                  header = FALSE, col.names = c("N1")),
+  s   = read.csv2(paste0(data.path, 'erstglieder/erstglieder+s.txt'),
+                 header = FALSE, col.names = c("N1")),
+  Ue  = read.csv2(paste0(data.path, 'erstglieder/erstglieder+Ue.txt'),
+                  header = FALSE, col.names = c("N1")),
+  Uer = read.csv2(paste0(data.path, 'erstglieder/erstglieder+Uer.txt'),
+                  header = FALSE, col.names = c("N1"))
+)
 
-e1token.nole <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+.csv'),
-                          sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1token.e <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+e.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1token.en <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+en.csv'),
-                        sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1token.er <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+er.csv'),
-                        sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1token.n <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+n.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1token.s <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+s.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1token.Ue <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+Ue.csv'),
-                        sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1token.Uer <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+Uer.csv'),
-                         sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-# e1token.Un <- read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+Un.csv'),
-#                         sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))  # No matches.
 
-e1type.nole <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+.csv'),
-                         sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1type.e <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+e.csv'),
-                      sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1type.en <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+en.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1type.er <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+er.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1type.n <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+n.csv'),
-                      sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1type.s <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+s.csv'),
-                      sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1type.Ue <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+Ue.csv'),
-                       sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-e1type.Uer <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+Uer.csv'),
-                        sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))
-# e1type.Un <- read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+Un.csv'),
-#                         sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("Noun", "Frequency"))  # No matches.
+fhapax <- list(
+  no  = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  e   = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+e.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  en  = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+en.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  er  = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+er.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  n   = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+n.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  s   = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+s.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  Ue  = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+Ue.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  Uer  = read.csv2(paste0(data.path, 'fhapax/fhapax_erstglieder+Uer.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F"))
+)
 
-e1compounds.nole <- read.csv2(paste0(data.path, 'compounds/compounds+.csv'),
-                              sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-e1compounds.e <- read.csv2(paste0(data.path, 'compounds/compounds+e.csv'),
-                           sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-e1compounds.en <- read.csv2(paste0(data.path, 'compounds/compounds+en.csv'),
-                            sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-e1compounds.er <- read.csv2(paste0(data.path, 'compounds/compounds+er.csv'),
-                            sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-e1compounds.n <- read.csv2(paste0(data.path, 'compounds/compounds+n.csv'),
-                           sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-e1compounds.s <- read.csv2(paste0(data.path, 'compounds/compounds+s.csv'),
-                           sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-e1compounds.Ue <- read.csv2(paste0(data.path, 'compounds/compounds+Ue.csv'),
-                            sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-e1compounds.Uer <- read.csv2(paste0(data.path, 'compounds/compounds+Uer.csv'),
-                             sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))
-# e1compounds.Un <- read.csv2(paste0(data.path, 'compounds/compounds+Un.csv'),
-#                         sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("Noun1", "Noun2", "Frequency"))  # No matches.
+
+ftoken <- list(
+  no  = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  e   = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+e.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  en  = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+en.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  er  = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+er.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  n   = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+n.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  s   = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+s.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  Ue  = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+Ue.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  Uer = read.csv2(paste0(data.path, 'ftoken/ftoken_erstglieder+Uer.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F"))
+)
+
+
+ftype <- list(
+  no  = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  e   = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+e.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  en  = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+en.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  er  = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+er.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  n   = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+n.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  s   = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+s.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  Ue  = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+Ue.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  Uer = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+Uer.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F")),
+  Uen = read.csv2(paste0(data.path, 'ftype/ftype_erstglieder+Uer.csv'),
+                  sep = "\t", colClasses = c("character", "integer"), header = FALSE, col.names = c("N1", "F"))
+)
+
+
+compounds <- list(
+  no  = read.csv2(paste0(data.path, 'compounds/compounds+.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F")),
+  e   = read.csv2(paste0(data.path, 'compounds/compounds+e.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F")),
+  en  = read.csv2(paste0(data.path, 'compounds/compounds+en.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F")),
+  er  = read.csv2(paste0(data.path, 'compounds/compounds+er.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F")),
+  n   = read.csv2(paste0(data.path, 'compounds/compounds+n.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F")),
+  s   = read.csv2(paste0(data.path, 'compounds/compounds+s.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F")),
+  Ue  = read.csv2(paste0(data.path, 'compounds/compounds+Ue.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F")),
+  Uer = read.csv2(paste0(data.path, 'compounds/compounds+Uer.csv'),
+                  sep = "\t", colClasses = c("character", "character", "integer"), header = FALSE, col.names = c("N1", "N2", "F"))
+)
+
+
+blacklists <- list(
+  no  = data.frame(N1 = c("Nonwort")),
+  e   = read.csv2(paste0(data.path, 'erstglieder/real_blacklist+e.txt'),
+                  sep = "\t", colClasses = c("character"), header = FALSE, col.names = c("N1")),
+  en  = read.csv2(paste0(data.path, 'erstglieder/real_blacklist+en.txt'),
+                  sep = "\t", colClasses = c("character"), header = FALSE, col.names = c("N1")),
+  er  = read.csv2(paste0(data.path, 'erstglieder/real_blacklist+er.txt'),
+                  sep = "\t", colClasses = c("character"), header = FALSE, col.names = c("N1")),
+  n   = read.csv2(paste0(data.path, 'erstglieder/real_blacklist+n.txt'),
+                  sep = "\t", colClasses = c("character"), header = FALSE, col.names = c("N1")),
+  s   = data.frame(N1 = c("Nonwort")),
+  Ue  = read.csv2(paste0(data.path, 'erstglieder/real_blacklist+Ue.txt'),
+                  sep = "\t", colClasses = c("character"), header = FALSE, col.names = c("N1")),
+  Uer = read.csv2(paste0(data.path, 'erstglieder/real_blacklist+Uer.txt'),
+                  sep = "\t", colClasses = c("character"), header = FALSE, col.names = c("N1")),
+  Uen = read.csv2(paste0(data.path, 'erstglieder/real_blacklist+Uer.txt'),
+                  sep = "\t", colClasses = c("character"), header = FALSE, col.names = c("N1"))
+)
--- Data/R/productivity.R
+++ Data/R/productivity.R
@@ -11,18 +11,60 @@ setwd(script.path)
 
 source('load.R')
 
-plot(e1token.nole$Frequency~e1type.nole$Frequency, type="l", lwd = lwd, lty = lty[1], col = col[1],
-     ylim = c(1, 3400000), log = "xy",
+
+# Get rid of blacklisted N1s.
+clean.df.by.blacklist <- function(df, blacklist, column) {
+  .blacked <- match(df[,column], blacklist[,column])
+  .blacked <- .blacked[which(!is.na(.blacked))]
+  if (length(.blacked) > 0) {
+    df[-c(.blacked),]
+  } else {
+    df
+  }
+}
+
+clean.dfs.by.blacklist <- function(lst, blacklst, column) {
+  .res = list()
+  for (dfn in names(lst)) {
+    .r <- clean.df.by.blacklist(lst[[dfn]], blacklst[[dfn]], column)
+    .res[[dfn]] <- .r
+  }
+  .res
+}
+
+nouns <- clean.dfs.by.blacklist(nouns, blacklists, "N1")
+ftype <- clean.dfs.by.blacklist(ftype, blacklists, "N1")
+ftoken <- clean.dfs.by.blacklist(ftoken, blacklists, "N1")
+fhapax <- clean.dfs.by.blacklist(fhapax, blacklists, "N1")
+compounds <- clean.dfs.by.blacklist(compounds, blacklists, "N1")
+
+plot(ftoken[['no']]$F~ftype[['no']]$F, type="l", lwd = lwd, lty = lty[1], col = col[1],
+     ylim = c(1, 3400000),
      main = "Type & token frequency of N1",
-     xlab = "type frequency (log)",
-     ylab = "token frequency (log)")
-lines(e1token.s$Frequency~e1type.s$Frequency, type="l", lwd = lwd, lty = lty[2], col = col[2])
-lines(e1token.e$Frequency~e1type.e$Frequency, type="l", lwd = lwd, lty = lty[3], col = col[3])
-lines(e1token.en$Frequency~e1type.en$Frequency, type="l", lwd = lwd, lty = lty[4], col = col[4])
-lines(e1token.er$Frequency~e1type.er$Frequency, type="l", lwd = lwd, lty = lty[5], col = col[5])
-lines(e1token.n$Frequency~e1type.n$Frequency, type="l", lwd = lwd, lty = lty[6], col = col[6])
-lines(e1token.Ue$Frequency~e1type.Ue$Frequency, type="l", lwd = lwd, lty = lty[7], col = col[7])
-lines(e1token.Uer$Frequency~e1type.Uer$Frequency, type="l", lwd = lwd, lty = lty[8], col = col[8])
+     xlab = "type frequency",
+     ylab = "token frequency")
+lines(ftoken[['s']]$F~ftype[['s']]$F, type="l", lwd = lwd, lty = lty[2], col = col[2])
+lines(ftoken[['e']]$F~ftype[['e']]$F, type="l", lwd = lwd, lty = lty[3], col = col[3])
+lines(ftoken[['en']]$F~ftype[['en']]$F, type="l", lwd = lwd, lty = lty[4], col = col[4])
+lines(ftoken[['er']]$F~ftype[['er']]$F, type="l", lwd = lwd, lty = lty[5], col = col[5])
+lines(ftoken[['n']]$F~ftype[['n']]$F, type="l", lwd = lwd, lty = lty[6], col = col[6])
+lines(ftoken[['Ue']]$F~ftype[['Ue']]$F, type="l", lwd = lwd, lty = lty[7], col = col[7])
+lines(ftoken[['Uer']]$F~ftype[['Uer']]$F, type="l", lwd = lwd, lty = lty[8], col = col[8])
 legend('topleft', lwd = lwd, col = col, lty = lty,
         legend = c("0", "-s", "-e", "-en", "-er", "-n", "=e", "=er"))
 
+
+plot(ftoken[['no']]$F~ftype[['no']]$F, type="l", lwd = lwd, lty = lty[1], col = col[1],
+     ylim = c(1, 100), xlim = c(1,30),
+     main = "Type & token frequency of N1 (Closeup)",
+     xlab = "type frequency",
+     ylab = "token frequency")
+lines(ftoken[['s']]$F~ftype[['s']]$F, type="l", lwd = lwd, lty = lty[2], col = col[2])
+lines(ftoken[['e']]$F~ftype[['e']]$F, type="l", lwd = lwd, lty = lty[3], col = col[3])
+lines(ftoken[['en']]$F~ftype[['en']]$F, type="l", lwd = lwd, lty = lty[4], col = col[4])
+lines(ftoken[['er']]$F~ftype[['er']]$F, type="l", lwd = lwd, lty = lty[5], col = col[5])
+lines(ftoken[['n']]$F~ftype[['n']]$F, type="l", lwd = lwd, lty = lty[6], col = col[6])
+lines(ftoken[['Ue']]$F~ftype[['Ue']]$F, type="l", lwd = lwd, lty = lty[7], col = col[7])
+lines(ftoken[['Uer']]$F~ftype[['Uer']]$F, type="l", lwd = lwd, lty = lty[8], col = col[8])
+legend('topleft', lwd = lwd, col = col, lty = lty,
+        legend = c("0", "-s", "-e", "-en", "-er", "-n", "=e", "=er"))
