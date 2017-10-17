bc386f5fb5234bab0a69ebc63b48eaaf8163901a
Roland Schäfer <roland.schaefer@fu-berlin.de>
2017-10-17 21:35:41 +0200

    Polished productivity scripts.

--- Data/Database/compounds/get_bigfiles.sh
+++ Data/Database/compounds/get_bigfiles.sh
@@ -7,5 +7,6 @@ wget https://www.webcorpora.org/projectdata/decowcompounds/compounds+.csv.gz
 wget https://www.webcorpora.org/projectdata/decowcompounds/compounds+s.csv.gz
 wget https://www.webcorpora.org/projectdata/decowcompounds/decow16ax_comps.txt.gz
 wget https://www.webcorpora.org/projectdata/decowcompounds/decow16ax_comps_counts.txt.gz
+wget https://www.webcorpora.org/projectdata/decowcompounds/decow16ax_nouns_counts.csv.gz
 
-gunzip compounds+.csv.gz compounds+s.csv.gz decow16ax_comps.txt.gz decow16ax_comps_counts.txt.gz
+gunzip compounds+.csv.gz compounds+s.csv.gz decow16ax_comps.txt.gz decow16ax_comps_counts.txt.gz decow16ax_nouns_counts.csv.gz
--- Data/R/productivity.R
+++ Data/R/productivity.R
@@ -6,7 +6,7 @@ lwd <- 3
 lty <- 1:10
 col <- 1:10
 
-my.colors = colorRampPalette(c("yellow", "black"))(100)
+my.colors = colorRampPalette(c("yellow", "darkred"))(100)
 
 source('load.R')
 
@@ -94,55 +94,117 @@ map.log <-  function(x, x.max, to.max = 2) {
 }
 
 
-# Get N1s used with and without LE.
+make.full.analysis <-  function(le, prods) {
+    .n1s <- merge(prods[[le]], prods$no, by = 'N1')
+    colnames(.n1s) <- c('N1',
+                        paste('With', colnames(prods[[le]]), sep = '_')[-1],
+                        paste('Without', colnames(prods[[le]]), sep = '_')[-1]
+                        )
+    .n1s
+}
+
+plot.productivities <- function(le, analyses, dots = F, max.plottable = 100, norm.xax = NULL, norm.yax = NULL) {
+
+  .n1s <- analyses[[le]]
+
+  # Reduce number of plottables if too many.
+  if (max.plottable > 0 & nrow(.n1s) > max.plottable) {
+    .subtitle <- paste0("\n(", nrow(.n1s) - max.plottable, " of ", nrow(.n1s), " not shown)")
+    .n1s <- .n1s[sample(1:nrow(.n1s), max.plottable, replace = F),]
+  } else {
+    .subtitle <- ''
+  }
+
+  # For font size, get maximum token frequency and mapping freq -> font size.
+  .fty.max.with     <- max(.n1s$With_Ftype)
+  .fty.max.without  <- max(.n1s$Without_Ftype)
+  .prod.with.max    <- max(.n1s$With_Ppot)
+  .prod.with.min    <- min(.n1s$With_Ppot)
+  .prod.without.max <- max(.n1s$Without_Ppot)
+  .prod.without.min <- min(.n1s$Without_Ppot)
+
+  if (!is.null(norm.xax)) {
+    .xlim <- norm.xax
+  } else {
+    .xlim <- c(.prod.with.min*0.75, .prod.with.max*1.25)
+  }
+  if (!is.null(norm.yax)) {
+    .ylim <- norm.yax
+  } else {
+    .ylim <- c(.prod.without.min*0.75, .prod.without.max*1.25)
+  }
+
+  .le.name <- ifelse(startsWith(le, 'U'), paste0('=', substr(le, 2, nchar(le))), paste0('-', le))
+
+  plot(.n1s[,"With_Ppot"]~.n1s[,"Without_Ppot"], type="n",
+       main = paste0("Productivity of N1 with ", .le.name, .subtitle),
+       xlim = .xlim,
+       ylim = .ylim,
+       ylab = "without linking element",
+       xlab = paste0("with ", .le.name, " linking element"),
+       log = "xy"
+       )
+  for (n in 1:nrow(.n1s)) {
+    if (dots) {
+      points(.n1s[n,"With_Ppot"], .n1s[n,"Without_Ppot"],
+             pch = 20,
+             cex = map.log(.n1s[n,"With_Ftype"], .fty.max.with, 4),
+             col = my.colors[round(map.log(.n1s[n,"Without_Ftype"], .fty.max.without, 100), 0)]
+            )
+     legend("topleft", legend = c(paste0("fty=1;1"), paste0("fty=", .fty.max.with, ";", .fty.max.without)),
+            pch = 20,
+            col = c(my.colors[round(map.log(2, .fty.max.without, 100), 0)],
+                    my.colors[round(map.log(.fty.max.without, .fty.max.without, 100), 0)]),
+            pt.cex = c(1, map.log(.fty.max.with, .fty.max.with, 4)),
+            cex = 0.8
+            )
+    } else {
+      text(.n1s[n,"With_Ppot"], .n1s[n,"Without_Ppot"],
+           cex = map.log(.n1s[n,"With_Ftype"], .fty.max.with),
+           labels = .n1s[n, "N1"],
+           col = my.colors[round(map.log(.n1s[n,"Without_Ftype"], .fty.max.without, 100), 0)],
+           srt = sample(-90:90, 1)
+           )
+    }
+  }
+}
+
+
+# Make complete data frames.
 
 analyses.full <- list(
   e   = NULL, en  = NULL, er  = NULL,
   n   = NULL, Ue  = NULL, Uer = NULL
 )
 
-max.plottable <- 100
-
 for (le in c('e', 'Ue', 'er', 'Uer', 'n', 'en')) {
-  .n1s <- merge(productivities[[le]], productivities$no, by = 'N1')
-  colnames(.n1s) <- c('N1',
-                      paste('With', colnames(productivities$er), sep = '_')[-1],
-                      paste('Without', colnames(productivities$er), sep = '_')[-1]
-                      )
-
-  # Add to persistent list.
-  analyses.full[[le]] <- .n1s
-
-  # Reduce number of plottables if too many.
-  if (nrow(.n1s) > max.plottable) {
-    .n1s <- .n1s[sample(1:nrow(.n1s), max.plottable, replace = F),]
-    .reduced <- T
-  } else .reduced <- F
-
-  # For font size, get maximum token frequency and mapping freq -> font size.
-  .fto.max          <- max(.n1s$With_Ftoken)
-  .fty.max          <- max(.n1s$With_Ftype)
-  .prod.with.max    <- max(.n1s$With_Ppot)
-  .prod.with.min    <- min(.n1s$With_Ppot)
-  .prod.without.max <- max(.n1s$Without_Ppot)
-  .prod.without.min <- min(.n1s$Without_Ppot)
-
-  .le.name <- ifelse(startsWith(le, 'U'), paste0('=', substr(le, 2, nchar(le))), paste0('-', le))
-
-  plot(.n1s[,"With_Ppot"]~.n1s[,"Without_Ppot"], type="n",
-       main = paste0("Potential productivity P of N1 with ", .le.name, " and -0"),
-       xlim = c(.prod.with.min*0.75, .prod.with.max*1.25),
-       ylim = c(.prod.without.min*0.75, .prod.without.max*1.25),
-       ylab = "P[pot](N1-0)",
-       xlab = paste0("P[pot](N1", .le.name, ")"),
-       log = "xy"
-       )
-  for (n in 1:nrow(.n1s)) {
-    text(.n1s[n,"With_Ppot"], .n1s[n,"Without_Ppot"],
-         cex = map.log(.n1s[n,"With_Ftoken"], .fto.max),
-         labels = .n1s[n, "N1"],
-         col = my.colors[round(map.log(.n1s[n,"With_Ftype"], .fty.max, 100), 0)],
-         srt = sample(-90:90, 1)
-         )
-  }
+  analyses.full[[le]] <- make.full.analysis(le, productivities)
 }
+
+save(list = "analyses.full", file = "analyses.full.RData")
+
+# For debugging and after RStudio crashes.
+# load("analyses.full.RData")
+
+# Plot everything.
+options(scipen=999)
+plot.productivities('e', analyses.full)
+plot.productivities('Ue', analyses.full)
+plot.productivities('er', analyses.full)
+plot.productivities('Uer', analyses.full)
+plot.productivities('n', analyses.full)
+plot.productivities('en', analyses.full)
+options(scipen=0)
+
+options(scipen=999)
+pdf("productivity.pdf")
+par(mfrow=c(2,3))
+plot.productivities('e', analyses.full, dots = T, max.plottable = -1, norm.xax = c(0.0001,1), norm.yax = c(0.000001,1))
+plot.productivities('Ue', analyses.full, dots = T, max.plottable = -1, norm.xax = c(0.0001,1), norm.yax = c(0.000001,1))
+plot.productivities('er', analyses.full, dots = T, max.plottable = -1, norm.xax = c(0.0001,1), norm.yax = c(0.000001,1))
+plot.productivities('Uer', analyses.full, dots = T, max.plottable = -1, norm.xax = c(0.0001,1), norm.yax = c(0.000001,1))
+plot.productivities('n', analyses.full, dots = T, max.plottable = -1, norm.xax = c(0.0001,1), norm.yax = c(0.000001,1))
+plot.productivities('en', analyses.full, dots = T, max.plottable = -1, norm.xax = c(0.0001,1), norm.yax = c(0.000001,1))
+par(mfrow=c(1,1))
+dev.off()
+options(scipen=0)
