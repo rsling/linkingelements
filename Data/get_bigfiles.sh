76d8989d58719f5f145f5654fbd3872126e006e1
Roland Schäfer <roland.schaefer@fu-berlin.de>
2017-10-28 22:30:10 +0200

    Fix to download script.

--- Data/get_bigfiles.sh
+++ Data/get_bigfiles.sh
@@ -9,20 +9,20 @@ LOCALR="R/RData/"
 FILES=( "compounds+.csv.gz" "compounds+s.csv.gz" "decow16ax_comps.txt.gz" "decow16ax_comps_counts.txt.gz" "decow16ax_nouns_counts.csv.gz" )
 RFILES=( "noun.frequencies.RData" )
 
-# for f in ${FILES[@]}
-# do
-#   echo 
-#   echo "REMOTE: ${SERVER}${f} -> LOCAL: ${LOCAL}${f}"
-#   echo "Downloading..."
-#   wget "${SERVER}${f}" -O "${LOCAL}${f}"
-#   echo "Unzipping..."
-#   gunzip -f "${LOCAL}${f}"
-# done
+for f in ${FILES[@]}
+do
+  echo 
+  echo "REMOTE: ${SERVER}${f} -> LOCAL: ${LOCAL}${f}"
+  echo "Downloading..."
+  wget "${SERVER}${f}" -O "${LOCAL}${f}"
+  echo "Unzipping..."
+  gunzip -f "${LOCAL}${f}"
+done
 
 for f in ${RFILES[@]}
 do
   echo 
   echo "REMOTE: ${SERVER}${f} -> LOCAL: ${LOCALR}${f}"
   echo "Downloading..."
-  wget "${SERVER}${f}" -O "${LOCAL}${f}"
+  wget "${SERVER}${f}" -O "${LOCALR}${f}"
 done
